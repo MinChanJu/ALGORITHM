03f7efcfd3ec4a73988fc4dcf241c2cafa09921c
MinChanJu <mcj00220077@gmail.com>
2024-12-28 08:44:55 +0900

    수정

--- BAEKJOON(Python)/step 29/1520 G3.py
+++ BAEKJOON(Python)/step 29/1520 G3.py
@@ -1,25 +1,23 @@
 import sys
-input = sys.stdin.readline()
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
 M, N = map(int, input().split())
-graph = []
+board = []
 for _ in range(M):
-    graph.append(list(map(int, input().split())))
+    board.append(list(map(int, input().split())))
 dp = [[-1] * N for _ in range(M)]
 
-xd = [-1, 1, 0, 0]
-yd = [0, 0, -1, 1]
+dx, dy = [1, 0, 0, -1], [0, 1, -1, 0]
 
 def DFS(x, y):
-    if x == M-1 and y == N-1:
-        return 1
-    if dp[x][y] != -1:
-        return dp[x][y]
+    if x == M-1 and y == N-1: return 1
+    if dp[x][y] != -1: return dp[x][y]
     dp[x][y] = 0
     for i in range(4):
-        nx, ny = xd[i] + x, yd[i] + y
-        if nx >= 0 and nx < M and ny >= 0 and ny < N:
-            if graph[nx][ny] < graph[x][y]:
+        nx, ny = dx[i] + x, dy[i] + y
+        if 0 <= nx < M and 0 <= ny < N:
+            if board[nx][ny] < board[x][y]:
                 dp[x][y] += DFS(nx, ny)
     return dp[x][y]
 
--- BAEKJOON(Python)/step 42/3665 G1 (미완성).py
+++ BAEKJOON(Python)/step 42/3665 G1 (미완성).py
@@ -1,12 +1,45 @@
+from functools import cmp_to_key
 import sys
 input = sys.stdin.readline
 
+answer = []
 T = int(input())
 for _ in range(T):
     n = int(input())
     t = list(map(int,input().split()))
+    
+    tier = {}
+    for i in range(n): tier[t[i]] = i+1
+    
     m = int(input())
-    order = []
+    graph = {i:[] for i in range(1, n+1)}
     for i in range(m):
         a, b = map(int,input().split())
-        order.append((a,b))
+        graph[a].append(b)
+        graph[b].append(a)
+        
+    def compare_students(a, b):
+        global graph, tier
+        if b in graph[a]: return tier[b] - tier[a]
+        else: return tier[a] - tier[b]
+    
+    t.sort(key=cmp_to_key(compare_students))
+    
+    check = True
+    for i in range(n):
+        for j in range(i+1, n):
+            if (tier[t[i]] > tier[t[j]]):
+                if (t[j] not in graph[t[i]]):
+                    check = False
+                    answer.append("IMPOSSIBLE")
+                    break
+        if (not check): break
+                    
+    if (check):
+        res = ""
+        for i in range(n):
+            res += str(t[i])
+            if (i != n-1): res += " "
+        answer.append(res)
+
+print(*answer, sep="\n")
